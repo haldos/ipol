653046d3fcc3e9d2989f6330c1b30b6c8f5ee808
Haldo Sponton <haldos@fing.edu.uy>
2011-09-22 11:16:02 -0300

    modified:   dsp/lab0.2/quinto2.c

--- dsp/lab0.2/quinto2.c
+++ dsp/lab0.2/quinto2.c
@@ -24,7 +24,7 @@ int main() {
     typedef union {
         tipo_bit bits;
         tipo_nibbles nibbles;
-        unsigned char byte;
+        unsigned char byte : 8;
     } registro;
     
     // Utilice el tipo registro creado anteriormente para modificar una posición 
@@ -32,9 +32,12 @@ int main() {
     //          1. defina una variable entera inicializada en cero.
     //          2. imprima su valor en hexadecimal.
     
-    typedef union{
-        registro byte1;
-        int valor;
+    typedef struct {
+        registro byte1,byte2,byte3,byte4;
+    } cuatro_bytes;
+    typedef union {
+        cuatro_bytes bytes;
+        unsigned int valor;
     } entero;
     entero num;
     num.valor = 0;
@@ -42,9 +45,101 @@ int main() {
     printf("Valor (dec): %d \n", num.valor);
     printf("Valor (hex): %#x \n", num.valor);
     // Intento modificar <valor> usando los structs definidos antes:
-    num.byte1.bits.bit0 = 1;
-    num.byte1.byte = '8'; // '8' = 56 en ASCII.
+    num.bytes.byte1.bits.bit0 = 1;
+    num.bytes.byte1.bits.bit7 = 1;
+    num.bytes.byte2.bits.bit0 = 1;
     printf("Final:\n");
     printf("Valor (dec): %d \n", num.valor);
     printf("Valor (hex): %#x \n", num.valor);
+    
+    printf("\n");
+    
+    int byte_to_mod = 0;
+    int bit_to_mod = 0;
+    int value = 0;
+    num.valor = 0;
+    printf("Se asignó nuevamente valor 0.\n");
+    while (byte_to_mod!=-1){
+        
+        switch (byte_to_mod) {
+            case 1:
+                switch (bit_to_mod) {
+                    case 0: num.bytes.byte1.bits.bit0 = value; break;
+                    case 1: num.bytes.byte1.bits.bit1 = value; break;
+                    case 2: num.bytes.byte1.bits.bit2 = value; break;
+                    case 3: num.bytes.byte1.bits.bit3 = value; break;
+                    case 4: num.bytes.byte1.bits.bit4 = value; break;
+                    case 5: num.bytes.byte1.bits.bit5 = value; break;
+                    case 6: num.bytes.byte1.bits.bit6 = value; break;
+                    case 7: num.bytes.byte1.bits.bit7 = value; break;
+                    default: break;
+                }
+                break;
+            case 2:
+                switch (bit_to_mod) {
+                    case 0: num.bytes.byte2.bits.bit0 = value; break;
+                    case 1: num.bytes.byte2.bits.bit1 = value; break;
+                    case 2: num.bytes.byte2.bits.bit2 = value; break;
+                    case 3: num.bytes.byte2.bits.bit3 = value; break;
+                    case 4: num.bytes.byte2.bits.bit4 = value; break;
+                    case 5: num.bytes.byte2.bits.bit5 = value; break;
+                    case 6: num.bytes.byte2.bits.bit6 = value; break;
+                    case 7: num.bytes.byte2.bits.bit7 = value; break;
+                    default: break;
+                }
+                break;
+            case 3:
+                switch (bit_to_mod) {
+                    case 0: num.bytes.byte3.bits.bit0 = value; break;
+                    case 1: num.bytes.byte3.bits.bit1 = value; break;
+                    case 2: num.bytes.byte3.bits.bit2 = value; break;
+                    case 3: num.bytes.byte3.bits.bit3 = value; break;
+                    case 4: num.bytes.byte3.bits.bit4 = value; break;
+                    case 5: num.bytes.byte3.bits.bit5 = value; break;
+                    case 6: num.bytes.byte3.bits.bit6 = value; break;
+                    case 7: num.bytes.byte3.bits.bit7 = value; break;
+                    default: break;
+                }
+                break;
+            case 4:
+                switch (bit_to_mod) {
+                    case 0: num.bytes.byte4.bits.bit0 = value; break;
+                    case 1: num.bytes.byte4.bits.bit1 = value; break;
+                    case 2: num.bytes.byte4.bits.bit2 = value; break;
+                    case 3: num.bytes.byte4.bits.bit3 = value; break;
+                    case 4: num.bytes.byte4.bits.bit4 = value; break;
+                    case 5: num.bytes.byte4.bits.bit5 = value; break;
+                    case 6: num.bytes.byte4.bits.bit6 = value; break;
+                    case 7: num.bytes.byte4.bits.bit7 = value; break;
+                    default: break;
+                }
+                break;
+            default:
+                break;
+                
+        }
+        
+        printf("num:\n");
+        printf("%x%x%x%x%x%x%x%x %x%x%x%x%x%x%x%x %x%x%x%x%x%x%x%x %x%x%x%x%x%x%x%x = %d\n",
+            num.bytes.byte4.bits.bit7,num.bytes.byte4.bits.bit6,num.bytes.byte4.bits.bit5,
+            num.bytes.byte4.bits.bit4,num.bytes.byte4.bits.bit3,num.bytes.byte4.bits.bit2,
+            num.bytes.byte4.bits.bit1,num.bytes.byte4.bits.bit0,num.bytes.byte3.bits.bit7,
+            num.bytes.byte3.bits.bit6,num.bytes.byte3.bits.bit5,num.bytes.byte3.bits.bit4,
+            num.bytes.byte3.bits.bit3,num.bytes.byte3.bits.bit2,num.bytes.byte3.bits.bit1,
+            num.bytes.byte3.bits.bit0,num.bytes.byte2.bits.bit7,num.bytes.byte2.bits.bit6,
+            num.bytes.byte2.bits.bit5,num.bytes.byte2.bits.bit4,num.bytes.byte2.bits.bit3,
+            num.bytes.byte2.bits.bit2,num.bytes.byte2.bits.bit1,num.bytes.byte2.bits.bit0,
+            num.bytes.byte1.bits.bit7,num.bytes.byte1.bits.bit6,num.bytes.byte1.bits.bit5,
+            num.bytes.byte1.bits.bit4,num.bytes.byte1.bits.bit3,num.bytes.byte1.bits.bit2,
+            num.bytes.byte1.bits.bit1,num.bytes.byte1.bits.bit0,num.valor);
+        
+        printf("\n");
+        printf("Byte a modificar:");
+        scanf("%d",&byte_to_mod);
+        printf("Bit a modificar:");
+        scanf("%d",&bit_to_mod);
+        printf("Valor para el bit a modificar:");
+        scanf("%d",&value);
+        
+    }
 } 
