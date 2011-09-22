8ae09c25eaa755d7a518a150cac025c16f78c5f7
Haldo Sponton <haldos@fing.edu.uy>
2011-09-22 09:45:53 -0300

    modified:   dsp/lab0.2/quinto2.c

--- dsp/lab0.2/quinto2.c
+++ dsp/lab0.2/quinto2.c
@@ -26,4 +26,25 @@ int main() {
         tipo_nibbles nibbles;
         unsigned char byte;
     } registro;
-}
+    
+    // Utilice el tipo registro creado anteriormente para modificar una posición 
+    // de memoria dada. Para ello siga los siguientes pasos:
+    //          1. defina una variable entera inicializada en cero.
+    //          2. imprima su valor en hexadecimal.
+    
+    typedef union{
+        registro byte1;
+        int valor;
+    } entero;
+    entero num;
+    num.valor = 0;
+    printf("Inicial:\n");
+    printf("Valor (dec): %d \n", num.valor);
+    printf("Valor (hex): %#x \n", num.valor);
+    // Intento modificar <valor> usando los structs definidos antes:
+    num.byte1.bits.bit0 = 1;
+    num.byte1.byte = '8'; // '8' = 56 en ASCII.
+    printf("Final:\n");
+    printf("Valor (dec): %d \n", num.valor);
+    printf("Valor (hex): %#x \n", num.valor);
+} 
