5c9690d9c9e3696825bce68c0674344ca131acca
Haldo Sponton <haldos@fing.edu.uy>
2011-09-27 11:58:32 -0300

    new file:   examples/doxygen/fir.c 	new file:   examples/doxygen/fir.h 	new file:   "examples/doxygen/genera_se\303\261ales.c" 	new file:   "examples/doxygen/genera_se\303\261ales.h" 	new file:   examples/doxygen/makefile 	new file:   examples/doxygen/test_fir.c

---
+++ examples/doxygen/test_fir.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "genera_señales.h" // Ya incluye al fir.h.
+
+void print_signal(sample_t *signal){
+    sample_t *pf = signal;
+    int index;
+    for (index=0;index<SIGNAL_LENGTH;index++){
+        printf("%1.3f ",*pf);
+        pf++;
+    }
+    printf("\n");
+}
+
+int main(){
+    
+    // Señales de entrada y salida:
+    sample_t entrada[SIGNAL_LENGTH];
+    sample_t salida[SIGNAL_LENGTH];
+    
+    // Coeficientes del FIR:
+    sample_t coefs[TAP_LENGTH];
+        coefs[0] = 0.25;
+        coefs[1] = 0.25;
+        coefs[2] = 0.25;
+        coefs[3] = 0.25;
+    
+    // Inicializo entrada:
+    //step(entrada,SIGNAL_LENGTH,SIGNAL_LENGTH/2,1);
+    impulse(entrada,SIGNAL_LENGTH);
+    printf("Entrada: \n");
+    print_signal(entrada);
+    //print_signal(entrada);
+    
+    // Inicializo FIR:
+    ini_fir(coefs);
+    
+    int i;
+    for(i=0;i<SIGNAL_LENGTH;i++){
+        salida[i] = fir(entrada[i]);
+    }
+    
+    printf("Salida: \n");
+    print_signal(salida);
+}
