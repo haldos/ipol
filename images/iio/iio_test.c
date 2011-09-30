581bd45f02eae1a37391cbd13c8f24bfd67a6552
Haldo Sponton <haldos@fing.edu.uy>
2011-09-30 15:19:19 -0300

    modified:   images/iio/iio_test.c

--- images/iio/iio_test.c
+++ images/iio/iio_test.c
@@ -2,15 +2,19 @@
 #include <stdio.h> // only for "fprintf"
 #include <stdlib.h> // only for "free"
 
-int main(int c, char *v[])
-{
-	int w, h, pixeldim;
-	float *x = iio_read_image_float_vec(v[1], &w, &h, &pixeldim);
-	fprintf(stderr, "got a %dx%d image with %d channels\n", w, h, pixeldim);
-        
+int main(int c, char *v[]){
+    
+    if (c!=3){
+        printf("Usage: %s input_image output_image\n",v[0]);
+    } else {
+        int w, h, pixeldim;
+        float *x = iio_read_image_float_vec(v[1], &w, &h, &pixeldim);
+        fprintf(stderr, "Got a %dx%d image with %d channels\n", w, h, pixeldim);
+
         // some processing here...
-        
-	iio_save_image_float_vec(v[2], x, w, h, pixeldim);
-	free(x);
-	return 0;
+
+        iio_save_image_float_vec(v[2], x, w, h, pixeldim);
+        free(x);
+        return 0;
+    }
 }
