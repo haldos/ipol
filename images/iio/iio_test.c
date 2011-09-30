751dc68b16d69596ff04d5f716e8190559e48a49
Haldo Sponton <haldos@fing.edu.uy>
2011-09-30 14:15:24 -0300

    modified:   images/iio/iio_test.c

--- images/iio/iio_test.c
+++ images/iio/iio_test.c
@@ -7,6 +7,9 @@ int main(int c, char *v[])
 	int w, h, pixeldim;
 	float *x = iio_read_image_float_vec(v[1], &w, &h, &pixeldim);
 	fprintf(stderr, "got a %dx%d image with %d channels\n", w, h, pixeldim);
+        
+        // some processing here...
+        
 	iio_save_image_float_vec(v[2], x, w, h, pixeldim);
 	free(x);
 	return 0;
