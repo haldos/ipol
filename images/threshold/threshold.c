199b998f1c364bac727aaa63caeec10db768ea3b
Haldo Sponton <haldos@fing.edu.uy>
2011-09-30 15:58:21 -0300

    modified:   images/threshold/threshold.c

--- images/threshold/threshold.c
+++ images/threshold/threshold.c
@@ -4,16 +4,27 @@
 
 int main(int c, char *v[]){
     
-    if (c!=3){
+    if (c!=4){
         printf("Usage: %s threshold input_image output_image\n",v[0]);
     } else {
         int w, h, pixeldim;
         float *x = iio_read_image_float_vec(v[2], &w, &h, &pixeldim);
         fprintf(stderr, "Got a %dx%d image with %d channels\n", w, h, pixeldim);
 
+        int th = atoi(v[1]);
         printf("w = %d\n",w);
         printf("h = %d\n",h);
         printf("pixeldim = %d\n",pixeldim);
+        printf("th = %d\n",th);
+        
+        int i;
+        for( i=0 ; i<w*h*pixeldim ; i++ ){
+            if ( x[i] <= th ){
+                x[i] = 0;
+            } else {
+                x[i] = 255;
+            }
+        }
 
         iio_save_image_float_vec(v[3], x, w, h, pixeldim);
         free(x);
