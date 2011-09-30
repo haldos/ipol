9a260050428c138221e7526bc3317c843fecdc3c
Haldo Sponton <haldos@fing.edu.uy>
2011-09-30 14:14:02 -0300

    new file:   images/iio/compilar 	new file:   images/iio/iio.c 	new file:   images/iio/iio.h 	new file:   images/iio/iio_test.c

---
+++ images/iio/iio_test.c
@@ -0,0 +1,13 @@
+#include "iio.h"
+#include <stdio.h> // only for "fprintf"
+#include <stdlib.h> // only for "free"
+
+int main(int c, char *v[])
+{
+	int w, h, pixeldim;
+	float *x = iio_read_image_float_vec(v[1], &w, &h, &pixeldim);
+	fprintf(stderr, "got a %dx%d image with %d channels\n", w, h, pixeldim);
+	iio_save_image_float_vec(v[2], x, w, h, pixeldim);
+	free(x);
+	return 0;
+}
