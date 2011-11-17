5dd391928cb4e01735d6e12a3e8038f13f47f558
Haldo Sponton <haldos@fing.edu.uy>
2011-11-17 10:55:30 -0200

    new file:   examples/valgrind/tercero.c

---
+++ examples/valgrind/tercero.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+	
+	int *a = malloc(sizeof(int) * 100);
+	int *b = malloc(sizeof(int) * 100);
+
+	free(a);
+	
+	return 0;
+}
