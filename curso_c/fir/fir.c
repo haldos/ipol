6c383af0f2409f0595c43724255dfab7acd9aed9
Juan Cardelino <juan.cardelino@gmail.com>
2014-04-28 20:20:30 -0300

    FIR: removed wrong pow call

--- curso_c/fir/fir.c
+++ curso_c/fir/fir.c
@@ -30,7 +30,6 @@ sample_t fir(sample_t muestra){
     sample_t muestra_out = 0;
     for(i=0;i<TAP_LENGTH;i++){
         muestra_out = muestra_out + a[TAP_LENGTH-i-1]*buff_in[p];
-        muestra_out=powf(muestra_out,2);
 		  update_p(1);
     }
     update_p(1);
