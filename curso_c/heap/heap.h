3a29cd08c1c0d2c50020cf12faed0a519d2a34a1
Juan Cardelino <juan.cardelino@gmail.com>
2016-10-19 05:44:26 -0300

    added doxygen comments and config file

--- curso_c/heap/heap.h
+++ curso_c/heap/heap.h
@@ -8,15 +8,18 @@ typedef struct
 {
 	int n_max;
 	int n;
+	///array de punteros a ITEM
 	ITEM **data;
 }HEAP;
 
+/** Obtiene el mínimo elemento del heap y lo elimina del mismo*/
 ITEM* heap_extractMin(HEAP* h);
+/** Reserva la memoria para almacenar los punteros e inicializa los atributos del heap*/
 void  heap_init(HEAP* h, int n_max);
 int   heap_isEmpty(HEAP* h);
 void  heap_addItem(HEAP* h, ITEM* it);
 void  heap_free(HEAP* h);
-void heap_moveDown(HEAP* h, int i);
-void heap_moveUp(HEAP* h, int i);
+void  heap_moveDown(HEAP* h, int i);
+void  heap_moveUp(HEAP* h, int i);
 
 void heap_print(HEAP* h);
